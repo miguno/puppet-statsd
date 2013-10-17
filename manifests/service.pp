794f23fc0cf5a1869af2afbb596613dc1bfd64da
Michael G. Noll <mnoll@verisign.com>
2013-10-17 11:13:12 +0200

    Simplify service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,18 +1,17 @@
 class statsd::service inherits statsd {
 
-  validate_re($service_ensure, '^(running|stopped)$', 'The $service_ensure value must be either "running" or "stopped"')
-  validate_bool($service_manage)
-
   if ($service_manage) {
+
     if $service_ensure == 'running' {
       $ensure_real = 'running'
       $enable_real = true
-    } else {
+    }
+    else {
       $ensure_real = 'stopped'
       $enable_real = false
     }
 
-    service { 'statsd-daemon':
+    service { 'statsd':
       name       => $service_name,
       ensure     => $ensure_real,
       enable     => $enable_real,
